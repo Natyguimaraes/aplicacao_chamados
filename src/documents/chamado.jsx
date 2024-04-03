ff15284130f953423fc734142ec6c3a07867bab9
Natyguimaraes <santosnath433@gmail.com>
2024-04-02 21:16:44 -0300

    começo do código

---
+++ src/documents/chamado.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'; //importando biblioteca para utilização dos efeitos especiais
+
+ function Chamado (){
+    const [autor, setAutor] = useState (['']);
+    const [local, setLocal] = useState (['']);
+    const [data, setData] = useState (['']);
+    const [problema, setProblema] = useState (['']);
+ }
+  
